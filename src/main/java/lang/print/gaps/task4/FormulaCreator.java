cebf58bbe2f4c2c4fc2a96a8462f7b76142523dd
Przemysław Lecyk <przemyslaw@lecyk.net>
2022-10-25 01:36:16 +0200

    primitives variables

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
+
+        int c = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(c);
     }
 }
